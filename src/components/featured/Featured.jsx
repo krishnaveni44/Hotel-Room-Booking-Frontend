2a89c3eb01a8481851c1fb652b241a1eed3bed83
krishnaveni44 <krishnaveni44@gmail.com>
2022-06-25 16:42:25 +0530

    Hotel Booking Frontend

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import useFetch from "../../hooks/useFetch";
+import "./featured.css";
+
+const Featured = () => {
+  const { loading } = useFetch(
+    "http://localhost:8800/api/hotels/countByCity?cities=chennai,newdelhi,mumbai"
+  );
+
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://i0.wp.com/oneday.travel/wp-content/uploads/one-day-chennai-to-mahabalipuram-and-kanchipuram-trip-sightseeing-tour-package-arjunar-penance-mahabalipuram.jpg?resize=750%2C500&ssl=1"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Chennai</h1>
+              <h2>369 properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://content.r9cdn.net/rimg/dimg/6d/77/0cc45283-city-32821-176ddb032b7.jpg?width=1200&height=630&crop=false"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>New Delhi</h1>
+              <h2>231 properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/India/Mumbai/gateway-of-india-mumbai-xlarge.jpg?imwidth=640"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Mumbai</h1>
+              <h2>60 properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,40 @@
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
+
+const PropertyList = () => {
+  const { data, loading } = useFetch(
+    "http://localhost:8800/api/hotels/countByType"
+  );
+
+  const images = [
+    "https://cdn0.weddingwire.in/vendor/8684/3_2/960/jpg/hotels-lemon-tree-premier-ahmedabad-hotel-space-3_15_298684-159497949283782.jpeg",
+    "https://c4.wallpaperflare.com/wallpaper/22/205/283/apartment-condo-design-home-wallpaper-preview.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/438/466/493/huvafen-fushi-spa-resort-hd-with-high-resolution-wallpaper-preview.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQowHoGGaPWUT1QyhBgX6fBCqnpnrccsVGv9Q&usqp=CAU",
+    "https://www.pixelstalk.net/wp-content/uploads/2016/08/Log-Cabin-HD-Image.jpg",
+  ];
+  return (
+    <div className="pList">
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="pListItem" key={i}>
+                <img src={img} alt="" className="pListImg" />
+                <div className="pListTitles">
+                  <h1>{data[i]?.type}</h1>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PropertyList;
